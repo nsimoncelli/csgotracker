071176736f8218cae61feb7f85476c1ec03124a7
Nick Simoncelli <nicksimoncelli@gmail.com>
2019-09-19 17:11:47 -0700

    added express static

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express')
 const app = express();
 var mysql = require('mysql');
 const router = express.Router();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
